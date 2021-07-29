ed1655838a96b66ab81130c6a6b5e8fe9dd64d0c
Steven_Klavins <stevenklavins94@gmail.com>
2021-07-29 20:40:39 +0100

    chore: added input to state and validation

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,23 +1,52 @@
-import React from 'react';
-import styles from './AddUser.module.css'
-import Card from '../UI/Card';
-import Button from '../UI/Button';
+import React, { useState } from "react";
+import styles from "./AddUser.module.css";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
 
 function AddUser() {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
 
-    const addUserHandler = (event) => {
-        event.preventDefault();
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0 )  {
+      return;
     }
+    if (+enteredAge < 1)  {
+        return;
+      }
+    console.log(enteredUsername + enteredAge);
+    setEnteredAge("");
+    setEnteredUsername("");
+  };
+
+  const userNameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
 
   return (
-    <Card className ={styles.input}>
-    <form onSubmit={addUserHandler}>
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text"/>
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={userNameChangeHandler}
+        />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number"/>
-        <Button type = 'submit'>Add User</Button>
-    </form>
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
+        <Button type="submit">Add User</Button>
+      </form>
     </Card>
   );
 }
