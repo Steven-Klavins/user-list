56fe4457868f666ca8fe1b76bc03ebcc35452301
Steven_Klavins <stevenklavins94@gmail.com>
2021-07-31 22:57:41 +0100

    chore: added error modal for invalid input

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,17 +2,27 @@ import React, { useState } from "react";
 import styles from "./AddUser.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
 function AddUser(props) {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0 )  {
+    if (enteredUsername.trim().length === 0)  {
+      setError({
+        title: "Invalid name or age",
+        message: "Please enter a valid name and age"
+      });
       return;
     }
     if (+enteredAge < 1)  {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid name and age"
+      });
         return;
       }
     props.onAddUser(enteredUsername, enteredAge);
@@ -28,7 +38,13 @@ function AddUser(props) {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
+    <div>
+    {error && <ErrorModal title={error.title} message = {error.message} onConfirm={errorHandler}/>}
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
@@ -48,6 +64,7 @@ function AddUser(props) {
         <Button type="submit">Add User</Button>
       </form>
     </Card>
+    </div>
   );
 }
 
