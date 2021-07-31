5abb8b99d256037a665320fdd02457d3f77f466d
Steven_Klavins <stevenklavins94@gmail.com>
2021-07-31 22:10:28 +0100

    feat: added user item component list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 
 function App() {
+
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (userName, userAge) => {
+    setUsersList((previousList) => {
+      return [...previousList ,{name: userName, age: userAge, key: Math.random().toString()}]
+    } );
+  }
+  
   return (
     <div>
-      <AddUser/>
+      <AddUser onAddUser = {addUserHandler}/>
+      <UsersList users = {usersList}/>
     </div>
   );
 }
